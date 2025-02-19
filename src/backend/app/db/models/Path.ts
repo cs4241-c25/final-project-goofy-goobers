6ac81f0a25263d13b685de0c6429e9ddc72e27c5
Zen Dignan <zen@dignan.dev>
2025-02-19 01:54:13 -0500

    correct implementation of array of refs

--- src/backend/app/db/models/Path.ts
+++ src/backend/app/db/models/Path.ts
@@ -10,7 +10,7 @@ const PathSchema = new Schema<IPath>({
   name: String,
   description: String,
   owner: { type: Schema.Types.ObjectId, ref: 'User' }, // FK to User
-  waypoints: { type: [Schema.Types.ObjectId], ref: 'Waypoint' }, // list of FKs to Waypoint
+  waypoints: [{ type: Schema.Types.ObjectId, ref: 'Waypoint' }], // list of FKs to Waypoint
 });
 
 export const Path = model<IPath>('Path', PathSchema);
