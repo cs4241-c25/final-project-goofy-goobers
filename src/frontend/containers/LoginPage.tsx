5d8c21cbcbf4b06694eed3c55c181fd78d468931
Zen Dignan <zen@dignan.dev>
2025-02-23 16:22:05 -0500

    feat(fe): Basic functionality wrapping

--- src/frontend/containers/LoginPage.tsx
+++ src/frontend/containers/LoginPage.tsx
@@ -1,6 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback, useContext } from 'react';
 import { Button } from 'reactstrap';
+import { UserContext, useUser } from '../services/providers';
 
 export const LoginPage: FC = () => {
-  return <Button>Login</Button>;
+  const { setUser } = useContext(UserContext);
+  const user = useUser();
+
+  const authenticate = useCallback(async () => {
+    const attempt = await api.authenticate('gompei', 'salisbury');
+
+    setUser(attempt);
+  }, [setUser]);
+
+  return (
+    <>
+      <Button
+        onClick={() => {
+          void authenticate();
+        }}
+      >
+        Login
+      </Button>
+      {user && <p>You are authenticated as {user.username}</p>}
+    </>
+  );
 };
