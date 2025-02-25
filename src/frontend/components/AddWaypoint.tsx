48e2fe4136af14f778100f60dcfd8685cbe576c9
Gabriel_O <55938114+gabrielolafs@users.noreply.github.com>
2025-02-25 13:53:46 -0500

    stupid and random

--- src/frontend/components/AddWaypoint.tsx
+++ src/frontend/components/AddWaypoint.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useCallback, useState } from 'react';
-import { Button, Input, Label } from 'reactstrap';
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
 import { captureError } from '../utils';
 
 interface AddWaypointProps {
@@ -11,16 +11,16 @@ interface AddWaypointProps {
 export const AddWaypoint: FC<AddWaypointProps> = ({ close, refresh, pathID }) => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
 
   const handleAdd = useCallback(() => {
     api
       .createWaypoint(pathID, {
         name: name,
         description: description,
-        latitude: parseFloat(latitude),
-        longitude: parseFloat(longitude),
+        latitude: latitude,
+        longitude: longitude,
       })
       .then(() => {
         refresh();
@@ -32,74 +32,79 @@ export const AddWaypoint: FC<AddWaypointProps> = ({ close, refresh, pathID }) =>
 
   return (
     // TODO: make a front end check to make sure their are no null feilds where their shouldnt be
-    <>
-      <Label for="name">Name: </Label>
-      <Input
-        type="text"
-        id="name"
-        name="name"
-        placeholder="Enter Name"
-        value={name}
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      />
+    <Form>
+      <FormGroup>
+        <Label for="name">Name: </Label>
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Enter Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
 
-      <br />
+        <br />
 
-      <Label for="description">Description: </Label>
-      <Input
-        type="text"
-        id="description"
-        name="description"
-        placeholder="Enter description (optional)"
-        onChange={(e) => {
-          setDescription(e.target.value);
-        }}
-      />
+        <Label for="description">Description: </Label>
+        <Input
+          type="text"
+          id="description"
+          name="description"
+          placeholder="Enter description (optional)"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
 
-      <br />
+        <br />
 
-      <Label for="latitude">Latitude: </Label>
-      <Input
-        type="text"
-        id="latitude"
-        name="latitude"
-        placeholder="Enter decimal of latitude here"
-        onChange={(e) => {
-          setLatitude(e.target.value);
-        }}
-      />
+        <Label for="latitude">Latitude: </Label>
+        <Input
+          type="text"
+          id="latitude"
+          name="latitude"
+          placeholder="Enter decimal of latitude here"
+          defaultValue={latitude}
+          onChange={(e) => {
+            setLatitude(parseFloat(e.target.value));
+          }}
+          required
+        />
 
-      <br />
+        <br />
 
-      <Label for="longitude">Longitude: </Label>
-      <Input
-        type="text"
-        id="longitude"
-        name="longitude"
-        placeholder="Enter decimal of longitude here"
-        onChange={(e) => {
-          setLongitude(e.target.value);
-        }}
-      />
+        <Label for="longitude">Longitude: </Label>
+        <Input
+          type="text"
+          id="longitude"
+          name="longitude"
+          placeholder="Enter decimal of longitude here"
+          defaultValue={longitude}
+          onChange={(e) => {
+            setLongitude(parseFloat(e.target.value));
+          }}
+          required
+        />
 
-      <br />
+        <br />
 
-      <Button
-        onClick={() => {
-          close();
-        }}
-      >
-        Cancel
-      </Button>
-      <Button
-        onClick={() => {
-          handleAdd();
-        }}
-      >
-        Save
-      </Button>
-    </>
+        <Button
+          onClick={() => {
+            close();
+          }}
+        >
+          Cancel
+        </Button>
+        <Button
+          onClick={() => {
+            handleAdd();
+          }}
+        >
+          Save
+        </Button>
+      </FormGroup>
+    </Form>
   );
 };
