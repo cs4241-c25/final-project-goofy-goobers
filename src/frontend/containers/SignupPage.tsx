e653774fc18152fd39bda172d8b11839105e7bc3
Daniel Blatner <dblatner@wpi.edu>
2025-02-26 18:04:34 -0500

    Init

---
+++ src/frontend/containers/SignupPage.tsx
@@ -0,0 +1,9 @@
+import React, { FC, useState } from 'react';
+
+export const SignupPage: FC = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return <></>;
+};
